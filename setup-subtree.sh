9785a967e23b2a8e38052b5a2f158d54a972cf03
snomiao <snomiao@gmail.com>
2022-02-12 09:37:54 +0800

    doc: sync schcal

--- setup-subtree.sh
+++ setup-subtree.sh
@@ -1,3 +1,4 @@
+# sync schcal
 git subtree add --prefix=packages/schcal git@github.com:snomiao/schcal master --squash
 git subtree pull --prefix=packages/schcal git@github.com:snomiao/schcal master --squash
 git subtree push --prefix=packages/schcal git@github.com:snomiao/schcal master
