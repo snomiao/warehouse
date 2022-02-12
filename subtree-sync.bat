7ea984eaf724bf0e82213dbcee8d805d8d128179
snomiao <snomiao@gmail.com>
2022-02-12 10:27:01 +0800

    chore: git subtree for go-to-sleep

---
+++ subtree-sync.bat
@@ -0,0 +1,10 @@
+# sync %repo%
+SET repo=schcal
+git subtree add --prefix=packages/%repo% git@github.com:snomiao/%repo% master --squash
+git subtree pull --prefix=packages/%repo% git@github.com:snomiao/%repo% master --squash
+git subtree push --prefix=packages/%repo% git@github.com:snomiao/%repo% master
+
+SET repo=go-to-sleep
+git subtree add --prefix=packages/%repo% git@github.com/snomiao/%repo% master --squash
+git subtree pull --prefix=packages/%repo% git@github.com:snomiao/%repo% master --squash
+git subtree push --prefix=packages/%repo% git@github.com:snomiao/%repo% master
--- subtree-sync.sh.bat
+++
@@ -1,4 +0,0 @@
-# sync schcal
-git subtree add --prefix=packages/schcal git@github.com:snomiao/schcal master --squash
-git subtree pull --prefix=packages/schcal git@github.com:snomiao/schcal master --squash
-git subtree push --prefix=packages/schcal git@github.com:snomiao/schcal master
